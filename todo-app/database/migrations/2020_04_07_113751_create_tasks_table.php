173b9d1055e7dc8441d0f5b5f981c71d5eaa48e7
chisato0805 <c23021705@gmail.com>
2020-04-07 12:08:23 +0000

    タスクテーブルのmigration追加

---
+++ todo-app/database/migrations/2020_04_07_113751_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('folder_id')->unsigned();
+            $table->string('title', 200);
+            $table->integer('status')->default(1);
+            $table->date('duedate');
+            $table->timestamps();
+
+            //外部キー制約
+            $table->foreign('folder_id')->references('id')->on('folders');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
