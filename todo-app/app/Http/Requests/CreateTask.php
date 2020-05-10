efa0a2f410f8941a585682c9d918d2d9eaa7d71b
chisato0805 <c23021705@gmail.com>
2020-05-10 09:49:20 +0000

    タスク作成追加

---
+++ todo-app/app/Http/Requests/CreateTask.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateTask extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'タイトル',
+            'due_date' => '期限日',
+        ];
+    }
+
+    public function message() {
+        return [
+            'due_date.after_or_equal' => ':attribute には今日以降の日付を入力してください。',
+        ];
+    }
+}
