e63437c3a9bcd7040923fd5bd3127e8f84818579
chisato0805 <c23021705@gmail.com>
2020-05-19 13:21:57 +0000

    認証追加

--- todo-app/app/Http/Controllers/FolderController.php
+++ todo-app/app/Http/Controllers/FolderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Folder;
 use App\Http\Requests\CreateFolder;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FolderController extends Controller
 {
