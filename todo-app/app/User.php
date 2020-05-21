9aac5e5c94a6f32cfd26030e9c172cbed3a9e70d
chisato0805 <c23021705@gmail.com>
2020-05-21 13:01:56 +0000

    バージョン変更

--- todo-app/app/User.php
+++ todo-app/app/User.php
@@ -2,14 +2,38 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable
 {
-    protected $fillable = ['user_id','name', 'email', 'password'];
+    use Notifiable;
 
-    public function folders()
-    {
-        return $this->hasMany('App\Folder');
-    }
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
--- todo-app/routes/web.php
+++ todo-app/routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,20 +11,6 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::group(['middleware' => 'auth'], function() {
-    Route::get('/', 'HomeController@index')->name('home');
-
-    Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
-
-    Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
-    Route::post('/folders/create', 'FolderController@create');
-
-    Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
-    Route::post('/folders/{id}/tasks/create', 'TaskController@create');
-
-    Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
-    Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
+Route::get('/', function () {
+    return view('welcome');
 });
-
-Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
