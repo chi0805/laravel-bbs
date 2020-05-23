4bcd4447861a528e65c9ee9a461cdf6f0b68cc10
chisato0805 <c23021705@gmail.com>
2020-05-23 11:52:15 +0000

    Folderモデルの読み込み追加

--- todo-app/app/Http/Controllers/TaskController.php
+++ todo-app/app/Http/Controllers/TaskController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Folder;
 
 class TaskController extends Controller
 {
     public function index()
     {
-        return "Hello world";
+        $folders = Folder::all();
+
+        return view('tasks/index', [
+            'folders' => $folders,
+        ]);
     }
 }
