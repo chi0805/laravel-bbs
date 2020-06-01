0a58fc1a102df992dea43f3a65dbd93b9ac43bcb
chisato0805 <c23021705@gmail.com>
2020-06-01 13:31:28 +0000

    投稿作成のルーティング追加

--- restaurants/routes/web.php
+++ restaurants/routes/web.php
@@ -14,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'HomeControllder@index')->name('home');
+Route::get('/records/create', 'RecordController@getCreate')->name('records.create');
