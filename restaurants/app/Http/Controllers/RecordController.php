0c43501631947e78385b4970ef9f261be6082a3c
chisato0805 <c23021705@gmail.com>
2020-06-03 13:14:58 +0000

    投稿作成コントローラ作成

--- restaurants/app/Http/Controllers/RecordController.php
+++ restaurants/app/Http/Controllers/RecordController.php
@@ -3,11 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Record;
 
 class RecordController extends Controller
 {
     public function getCreate()
     {
         return view('records/create');
+    }
+
+    public function postCreate(Request $request)
+    {
+        $record= new Record();
+
+        $record->title = $request->title;
+        $record->message = $request->message;
+
+        $record->save();
+
+        return redirect()->route('home');
     }
 }
