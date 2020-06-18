e2a831012846e6b39109be64977781e7fb661eb6
chisato0805 <c23021705@gmail.com>
2020-06-18 12:51:13 +0000

    ホーム画面

--- restaurants/resources/views/home.blade.php
+++ restaurants/resources/views/home.blade.php
@@ -3,19 +3,21 @@
 @section('content')
   <div class="container">
     <div class="row">
-      <div class="col col-md-9">
+      <div class="col col-md-12">
         <nav class="panel panel-default">
           <div class="panel-heading">投稿</div>
           <div class="panel-body">
           </div>
-          <div class="card">
+          <div class="card-deck ">
             @foreach($records as $record)
-              <div href="#" class="card-body">
-                <img src="{{ asset('images/test.JPG') }}" class="card-img-top" alt="カードの画像" style="width:80%;">
-                <h5 class="card-title">{{ $record->title }}</h5>
-                <p class="card-text">
-                  {{ $record->message }}
-                </p>
+              <div class="card record-item">
+                <div href="#" class="card-body">
+                  <img src="{{ asset('images/test.JPG') }}" class="card-img" alt="カードの画像" style="width:80%;">
+                  <h5 class="card-title">{{ $record->title }}</h5>
+                  <p class="card-text">
+                    {{ $record->message }}
+                  </p>
+                </div>
               </div>
             @endforeach
           </div>
