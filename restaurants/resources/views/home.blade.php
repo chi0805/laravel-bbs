56525dc33806b930293f6d96728aa5a55a824c14
chi0805 <c23021705@gmail.com>
2020-06-20 06:43:41 +0000

    投稿レイアウト修正

--- restaurants/resources/views/home.blade.php
+++ restaurants/resources/views/home.blade.php
@@ -6,22 +6,22 @@
             <div class="col col-md-12">
                 <nav class="panel panel-default">
                     <div class="panel-heading">投稿</div>
-                    <div class="panel-body">
-                    </div>
                     <div class="record-list">
                         <ul class="record-list-items">
                             @foreach($records as $record)
                                 <li class="record-item">
-                                    <img src="{{ asset('storage/images/' . $record->image) }}" class="record-item-img" alt="カードの画像" style="width:80%;">
+                                    <a href="#">
+                                        <img src="{{ asset('storage/images/' . $record->image) }}" class="record-item-image" alt="カードの画像">
+                                    </a>
                                     <div href="#" class="record-item-body">
                                         <h5 class="card-title">{{ $record->title }}</h5>
                                         <div class="record-item-tags">
                                             @foreach($record->tags as $tag)
-                                                <a href="#">{{ $record->tag }}</a>
+                                                <a href="#">{{ $tag }}</a>
                                             @endforeach
                                         </div>
                                     </div>
-                                </div>
+                                </li>
                             @endforeach
                     </div>
                 </nav>
