bda7f7868623e46adbede52833718ba18a16e409
chi0805 <c23021705@gmail.com>
2020-06-20 12:36:29 +0000

    グローバルナビに検索追加

--- restaurants/resources/views/layout.blade.php
+++ restaurants/resources/views/layout.blade.php
@@ -15,12 +15,15 @@
     <nav class="my-navbar">
         <a class="my-navbar-brand" href="/">Restaurants</a>
         <div class="my-navbar-control">
+            <a href="records/create" class="btn btn-light ">探す</a>
+            ｜
             <a href="records/create" class="btn btn-light ">投稿する</a>
             ｜
             <a href="#" id="logout" class="my-navbar-item">ログアウト</a>
             <form id="logout-form" action="#" method="POST" style="display: none;">
               @csrf
             </form>
+            ｜
             <a class="my-navbar-item" href="#">ログイン</a>
             ｜
             <a class="my-navbar-item" href="#">会員登録</a>
