8dd64c719da4931419d42c1baf492c873388ceeb
chi0805 <c23021705@gmail.com>
2020-06-20 03:28:02 +0000

    インデント

--- restaurants/resources/views/layout.blade.php
+++ restaurants/resources/views/layout.blade.php
@@ -1,32 +1,32 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Restaurants App</title>
-  @yield('styles')
-  <link rel="stylesheet" href="/css/styles.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Restaurants App</title>
+    @yield('styles')
+    <link rel="stylesheet" href="/css/styles.css">
 </head>
 <body>
 <header>
-  <nav class="my-navbar">
-    <a class="my-navbar-brand" href="/">Restaurants</a>
-    <div class="my-navbar-control">
-        <a href="records/create" class="btn btn-light ">投稿する</a>
-        ｜
-        <a href="#" id="logout" class="my-navbar-item">ログアウト</a>
-        <form id="logout-form" action="#" method="POST" style="display: none;">
-          @csrf
-        </form>
-        <a class="my-navbar-item" href="#">ログイン</a>
-        ｜
-        <a class="my-navbar-item" href="#">会員登録</a>
-    </div>
-  </nav>
+    <nav class="my-navbar">
+        <a class="my-navbar-brand" href="/">Restaurants</a>
+        <div class="my-navbar-control">
+            <a href="records/create" class="btn btn-light ">投稿する</a>
+            ｜
+            <a href="#" id="logout" class="my-navbar-item">ログアウト</a>
+            <form id="logout-form" action="#" method="POST" style="display: none;">
+              @csrf
+            </form>
+            <a class="my-navbar-item" href="#">ログイン</a>
+            ｜
+            <a class="my-navbar-item" href="#">会員登録</a>
+        </div>
+    </nav>
 </header>
 <main>
-  @yield('content')
+@yield('content')
 </main>
 @yield('scripts')
 </body>
--- restaurants/resources/views/records/create.blade.php
+++ restaurants/resources/views/records/create.blade.php
@@ -1,28 +1,28 @@
 @extends('layout')
 
 @section('content')
-  <div class="container">
-    <div class="row">
-      <div class="col col-md-offset-3 col-md-6">
-        <nav class="panel panel-default">
-          <div class="panel-heading">投稿を作成する</div>
-          <div class="panel-body">
-            <form action="#" method="post" enctype="multipart/form-data">
-              @csrf
-              <div class="form-group">
-                <label for="title">タイトル</label>
-                <input type="text" class="form-control" name="title" id="title" value="{{ old('title') }}" />
-                <label for="message">感想</label>
-                <input type="textarea" class="form-control" name="message" id="message" value="{{ old('message') }}" />
-                <input type="file" name="image">
-              </div>
-              <div class="text-right">
-                <button type="submit" class="btn btn-primary">送信</button>
-              </div>
-            </form>
-          </div>
-        </nav>
-      </div>
+    <div class="container">
+        <div class="row">
+            <div class="col col-md-offset-3 col-md-6">
+                <nav class="panel panel-default">
+                    <div class="panel-heading">投稿を作成する</div>
+                    <div class="panel-body">
+                        <form action="#" method="post" enctype="multipart/form-data">
+                            @csrf
+                            <div class="form-group">
+                                <label for="title">タイトル</label>
+                                <input type="text" class="form-control" name="title" id="title" value="{{ old('title') }}" />
+                                <label for="message">感想</label>
+                                <input type="textarea" class="form-control" name="message" id="message" value="{{ old('message') }}" />
+                                <input type="file" name="image">
+                            </div>
+                            <div class="text-right">
+                                <button type="submit" class="btn btn-primary">送信</button>
+                            </div>
+                        </form>
+                    </div>
+                </nav>
+            </div>
+        </div>
     </div>
-  </div>
 @endsection
